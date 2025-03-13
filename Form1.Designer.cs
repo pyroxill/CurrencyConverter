112e7d15688f9c701d1254a591db44e77454a5f1
Jayden Robertson <jaydensouterrobertson@gmail.com>
2025-03-13 22:08:14 +0000

    Upload Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelAmount = new System.Windows.Forms.Label();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.labelFromCurrency = new System.Windows.Forms.Label();
+            this.comboBoxFromCurrency = new System.Windows.Forms.ComboBox();
+            this.labelToCurrency = new System.Windows.Forms.Label();
+            this.comboBoxToCurrency = new System.Windows.Forms.ComboBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.labelConvertAmount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.White;
+            this.labelTitle.Location = new System.Drawing.Point(0, 0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(384, 23);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Currency Converter";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelAmount
+            // 
+            this.labelAmount.AutoSize = true;
+            this.labelAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAmount.ForeColor = System.Drawing.Color.White;
+            this.labelAmount.Location = new System.Drawing.Point(58, 44);
+            this.labelAmount.Name = "labelAmount";
+            this.labelAmount.Size = new System.Drawing.Size(112, 20);
+            this.labelAmount.TabIndex = 1;
+            this.labelAmount.Text = "Enter Amount:";
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxAmount.Location = new System.Drawing.Point(181, 44);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAmount.TabIndex = 2;
+            // 
+            // labelFromCurrency
+            // 
+            this.labelFromCurrency.AutoSize = true;
+            this.labelFromCurrency.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFromCurrency.ForeColor = System.Drawing.Color.White;
+            this.labelFromCurrency.Location = new System.Drawing.Point(58, 74);
+            this.labelFromCurrency.Name = "labelFromCurrency";
+            this.labelFromCurrency.Size = new System.Drawing.Size(117, 20);
+            this.labelFromCurrency.TabIndex = 3;
+            this.labelFromCurrency.Text = "From Currency:";
+            // 
+            // comboBoxFromCurrency
+            // 
+            this.comboBoxFromCurrency.FormattingEnabled = true;
+            this.comboBoxFromCurrency.Location = new System.Drawing.Point(181, 76);
+            this.comboBoxFromCurrency.Name = "comboBoxFromCurrency";
+            this.comboBoxFromCurrency.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxFromCurrency.TabIndex = 4;
+            // 
+            // labelToCurrency
+            // 
+            this.labelToCurrency.AutoSize = true;
+            this.labelToCurrency.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelToCurrency.ForeColor = System.Drawing.Color.White;
+            this.labelToCurrency.Location = new System.Drawing.Point(58, 104);
+            this.labelToCurrency.Name = "labelToCurrency";
+            this.labelToCurrency.Size = new System.Drawing.Size(98, 20);
+            this.labelToCurrency.TabIndex = 5;
+            this.labelToCurrency.Text = "To Currency:";
+            // 
+            // comboBoxToCurrency
+            // 
+            this.comboBoxToCurrency.FormattingEnabled = true;
+            this.comboBoxToCurrency.Location = new System.Drawing.Point(181, 106);
+            this.comboBoxToCurrency.Name = "comboBoxToCurrency";
+            this.comboBoxToCurrency.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxToCurrency.TabIndex = 6;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(95, 147);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 7;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(181, 147);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 8;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // labelConvertAmount
+            // 
+            this.labelConvertAmount.AutoSize = true;
+            this.labelConvertAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelConvertAmount.Location = new System.Drawing.Point(91, 194);
+            this.labelConvertAmount.Name = "labelConvertAmount";
+            this.labelConvertAmount.Size = new System.Drawing.Size(142, 20);
+            this.labelConvertAmount.TabIndex = 9;
+            this.labelConvertAmount.Text = "Converted Amount";
+            this.labelConvertAmount.Click += new System.EventHandler(this.labelConvertAmount_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.labelConvertAmount);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.comboBoxToCurrency);
+            this.Controls.Add(this.labelToCurrency);
+            this.Controls.Add(this.comboBoxFromCurrency);
+            this.Controls.Add(this.labelFromCurrency);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.labelAmount);
+            this.Controls.Add(this.labelTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelAmount;
+        private System.Windows.Forms.TextBox textBoxAmount;
+        private System.Windows.Forms.Label labelFromCurrency;
+        private System.Windows.Forms.ComboBox comboBoxFromCurrency;
+        private System.Windows.Forms.Label labelToCurrency;
+        private System.Windows.Forms.ComboBox comboBoxToCurrency;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelConvertAmount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json; //JSON Deserialize
+using RestSharp; //HTTP requests to the API
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace CurrencyConverter
+//Author: Jayden Robertson
+//Date: 13/03/2025
+//Description: Convert an amount from one currency to another using real-time exchange rates fetched from the CurrencyAPI service
+{
+    public partial class Form1 : Form
+    {
+        //Dictionary to store currency codes and their exchange rates
+        private Dictionary<string, double> currencies = new Dictionary<string, double>();
+        public Form1()
+        {
+            InitializeComponent(); 
+
+            //Get the latest currency exachange rates from the API
+            var currenciesJson = GetLatestCurrency();
+
+            //Deserialize the JSON into an object
+            dynamic responseObject = JsonConvert.DeserializeObject(currenciesJson);
+
+            //Loop through all the currencies and populate the combo boxes and dictionary
+            foreach (var item in responseObject.data)
+            {
+                var currency = item.Value;
+                this.comboBoxFromCurrency.Items.Add(currency.code.ToString());
+                this.comboBoxToCurrency.Items.Add(currency.code.ToString());
+                this.currencies.Add(currency.code.ToString(), (double)currency.value);
+            }
+        }
+
+        //Method to get the latest currency exchange rates from the API - code provided by API documentation
+        private string GetLatestCurrency()
+        {
+            var client = new RestClient("https://api.currencyapi.com/v3/latest");
+            var request = new RestRequest();
+            request.Method = Method.Get;
+            request.AddHeader("apikey", "cur_live_mCubawOnevrS2NGS0h443xFtYyth0VwahkfEmBFb");
+            RestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+        //Event handler for clear button 
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            //Clear all inputs and result label
+            this.textBoxAmount.Text = "";
+            this.comboBoxFromCurrency.Text = "";
+            this.comboBoxToCurrency.Text = "";
+            this.labelConvertAmount.Text = "";
+        }
+
+        //Label event handler
+        private void labelConvertAmount_Click(object sender, EventArgs e)
+        {
+            this.Text = ""; //This does nothing
+        }
+
+        //Event handler for convert button
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            var selectedFromCurrency = this.comboBoxFromCurrency.SelectedItem.ToString(); //Select from currency
+            var selectedToCurrency = this.comboBoxToCurrency.SelectedItem.ToString(); //Select to currency
+            var amount = double.Parse(this.textBoxAmount.Text); //Parse amount entered by the user
+
+            //Get the exchange rates for the selected currencies
+            var fromCurrencyExchangeRate = currencies[selectedFromCurrency];
+            var toCurrencyExchangeRate = currencies[selectedToCurrency];
+
+            //Calculate the converted amount
+            var calculatedAmount = amount * (toCurrencyExchangeRate / fromCurrencyExchangeRate);
+
+            this.labelConvertAmount.Text = "Result: " + calculatedAmount.ToString(); //Display the result back to the user
+        }
+    }
+}
